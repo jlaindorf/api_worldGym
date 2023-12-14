0dd307f46d2373fdf9f65f719001781dba830d6a
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-12-13 21:23:17 -0300

    Feat . Initialize student count

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index(Request $request){
 
+      $registered_students =   Student::where('user_id',$request->user()->id)->count();
+
+      return $registered_students;
     }
 }
