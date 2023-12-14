e455434764e1ec39eeae1dde78bde4c9408d26e4
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-12-14 16:43:07 -0300

    Feat. Introduce route, migration, table, and initialize controller

---
+++ app/Http/Controllers/ExerciseController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExerciseController extends Controller
+{
+    public function store(){
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,14 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ExerciseController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     // rotas privadas
     Route::get('dashboard',[DashboardController::class,'index']);
+    Route::post('exercises',[ExerciseController::class,'index']);
 });
 
 // rota pública
