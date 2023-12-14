bffbcbea50bc1dedb91b08dff8eca0ccb1faa7ce
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-12-14 20:30:24 -0300

    Feat. Create route to delete exercises

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('dashboard',[DashboardController::class,'index']);
     Route::post('exercises',[ExerciseController::class,'store']);
     Route::get('exercises',[ExerciseController::class,'index']);
+    Route::delete('exercises/{id}',[ExerciseController::class,'destroy']);
 });
 
 // rota pública
