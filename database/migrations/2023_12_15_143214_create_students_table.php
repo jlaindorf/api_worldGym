fbd8f6748bb7890d565aca5126b3ca198429fd96
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-12-15 19:53:49 -0300

    Feat. Implement students columns

--- database/migrations/2023_12_15_143214_create_students_table.php
+++ database/migrations/2023_12_15_143214_create_students_table.php
@@ -13,7 +13,23 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('name',255);
+            $table->string('email',255)->unique();
+            $table->date('date_birth');
+            $table->string('cpf',14)->unique();
+            $table->string('contact',20);
+            $table->unsignedInteger('user_id');
+            $table->string('city',50);
+            $table->string('neighborhood',50);
+            $table->string('number',30);
+            $table->string('street',30);
+            $table->string('state',2);
+            $table->string('cep',20);
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->SoftDeletes();
             $table->timestamps();
+
         });
     }
 
