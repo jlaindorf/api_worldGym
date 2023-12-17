e7b453b532d94e9545e6ec9f6cd1667b597edc59
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-12-17 20:10:14 -0300

    Feat: Add method to filter and list students by user_id

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -41,4 +41,43 @@ class StudentController extends Controller
             return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
         }
     }
+    public function index(Request $request){
+        try {
+
+            $filters = $request->query();
+            $students =Student::where('user_id', $request->user()->id)
+            ->select(
+                'students.id as student_id',
+                'students.name as student_name',
+                'students.email as student_email',
+                'students.cpf as student_cpf',
+                'students.contact as student_contact',
+                'students.cep as student_cep',
+                'students.street as student_street',
+                'students.state as student_state',
+                'students.neighborhood as student_neighborhood ',
+                'students.city as student_city',
+                'students.number as student_number',
+
+            );
+            if ($request->has('name') && !empty($filters['name'])) {
+                $students->where('name', 'ilike', '%' . $filters['name'] . '%');
+            }
+            if ($request->has('cpf') && !empty($filters['cpf'])) {
+                $students->where('cpf', 'ilike', '%' . $filters['cpf'] . '%');
+            }
+            if ($request->has('email') && !empty($filters['email'])) {
+                $students->where('email', 'ilike', '%' . $filters['email'] . '%');
+            }
+
+
+
+
+
+            return   $students->orderby('name','ASC')->get();
+
+        } catch (\Exception $exception) {
+            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+    }
 }
