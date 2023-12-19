de7332619fef9c4f3ed6cfe66425568d768280f9
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-12-18 21:06:32 -0300

    Add.initialize workour controller to method store

--- app/Http/Controllers/WorkoutController.php
+++ app/Http/Controllers/WorkoutController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Workout;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class WorkoutController extends Controller
 {
-    //
+    public function store(Request $request){
+        try{
+           $data = $request->all();
+            $request->validate([
+               'student_id' => 'integer',
+               'exercise_id' => 'integer',
+               'repetitions' => 'integer|required',
+               'weight' => 'float|required',
+               'break_time' => 'integer|required',
+               'day' => 'required|in:SEGUNDA,TERÇA,QUARTA,QUINTA,SEXTA,SÁBADO,DOMINGO',
+               'observations' => 'string|required',
+               'time' => 'integer|required'
+            ]);
+
+          $workout = Workout::create($data);
+
+          return $workout;
+
+
+        }catch (\Exception $exception) {
+            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+    }
 }
