5eb0951e7e8139412b87a51e7ac355a2244fea19
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-12-19 10:47:44 -0300

    Feat. Add method to store workout with right responses

--- app/Http/Controllers/WorkoutController.php
+++ app/Http/Controllers/WorkoutController.php
@@ -8,26 +8,35 @@ use Symfony\Component\HttpFoundation\Response;
 
 class WorkoutController extends Controller
 {
-    public function store(Request $request){
-        try{
-           $data = $request->all();
+    public function store(Request $request)
+    {
+        try {
+
+            $data = $request->all();
+
             $request->validate([
-               'student_id' => 'integer',
-               'exercise_id' => 'integer',
-               'repetitions' => 'integer|required',
-               'weight' => 'float|required',
-               'break_time' => 'integer|required',
-               'day' => 'required|in:SEGUNDA,TERÇA,QUARTA,QUINTA,SEXTA,SÁBADO,DOMINGO',
-               'observations' => 'string|required',
-               'time' => 'integer|required'
+                'student_id' => 'integer',
+                'exercise_id' => 'integer',
+                'repetitions' => 'integer|required',
+                'weight' => 'numeric|required',
+                'break_time' => 'integer|required',
+                'day' => 'required|in:SEGUNDA,TERÇA,QUARTA,QUINTA,SEXTA,SÁBADO,DOMINGO',
+                'observations' => 'string|required',
+                'time' => 'integer|required'
             ]);
 
-          $workout = Workout::create($data);
+            $sameWorkout = Workout::where([
+                'student_id' => $data['student_id'],
+                'exercise_id' => $data['exercise_id'],
+                'day' => $data['day']
+            ])->first();
+            if ($sameWorkout) {
+                return $this->response('Treino já existente para o aluno', Response::HTTP_CONFLICT);
+            }
+            $workout = Workout::create($data);
 
-          return $workout;
-
-
-        }catch (\Exception $exception) {
+            return $workout;
+        } catch (\Exception $exception) {
             return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
         }
     }
