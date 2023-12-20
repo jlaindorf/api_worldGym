79543dc66ef96579eab435f5a934b68a6d9afbec
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-12-20 20:25:59 -0300

    Fix. Set style of PDF

--- resources/views/pdfs/showWorkout.blade.php
+++ resources/views/pdfs/showWorkout.blade.php
@@ -19,29 +19,41 @@
 
     .day {
         font-size: 14px;
-        color: blue;
+        color: rgba(245, 245, 245);
+        background-color: black;
     }
 
     .list {
         font-size: 12px;
+        padding-top: 10px;
     }
+    img {
+            width: 80px;
+            position: absolute;
+            top: 0;
+            right: 0;
+            margin-right: 68%;
+            z-index: -1;
+        }
 </style>
 
 <head></head>
 
 <body>
 
+
+    <img  src="https://i.pinimg.com/originals/42/9d/63/429d631659a11a9eb666b103d811470a.jpg" alt="barra de peso" width="100">
     <h1 class="logo">WorldGym</h1>
     <h2 class="name">Treinos da semana <br>{{ $name }}</h2>
 
     @foreach ($workouts as $day => $dayWorkouts)
-        <h2 class="day">{{ $day }}</h2>
+        <h2 class="day" style="border: 1px solid black;">{{ $day }}</h2>
         @if (empty($dayWorkouts))
             <p class="list">Nenhum treino para este dia.</p>
         @else
-            <ul>
+        <ul >
                 @foreach ($dayWorkouts as $workout)
-                    <li class="list">
+                    <li class="list" >
                         <strong>Exercício:</strong> {{ $workout['exercise']['description'] }}
                         | <strong>Peso:</strong> {{ $workout['weight'] }} kg
                         | <strong>Tempo:</strong> {{ $workout['time'] }} minutos
